48cbbfb403cd222fa17c14132c2019861c819bc3
Björn Andersson <ba@sanitarium.se>
2021-11-13 21:22:16 +0800

    First neighbour calculation

---
+++ life/internal/gol/life.go
@@ -0,0 +1,22 @@
+package gol
+
+import "not-so-random/life"
+
+// Neighbours returns how many adjacent cells are alive
+func Neighbours(w life.World, row, col int) int {
+	var total int
+
+	if right := col+1; right < len(w.Grid[row]) {
+		total += w.Grid[row][right]
+	}
+
+	if left := col-1; left >= 0 {
+		total += w.Grid[row][left]
+	}
+
+	if above := row-1; above >= 0 {
+		total += w.Grid[above][col]
+	}
+
+	return total
+}
---
+++ life/internal/gol/life_test.go
@@ -0,0 +1,61 @@
+package gol_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"not-so-random/life"
+	"not-so-random/life/internal/gol"
+)
+
+func TestNeighbours(t *testing.T) {
+
+	testCases := []struct {
+		name     string
+		input    [][]int
+		row      int
+		col      int
+		expected int
+	}{
+		{
+			name:     "no neighbours is 0",
+			input:    [][]int{{0}},
+			row:      0,
+			col:      0,
+			expected: 0,
+		},
+		{
+			name:     "1 neighbour to the right",
+			input:    [][]int{{0, 1}},
+			row:      0,
+			col:      0,
+			expected: 1,
+		},
+		{
+			name:     "1 neighbour to the left",
+			input:    [][]int{{1, 0}},
+			row:      0,
+			col:      1,
+			expected: 1,
+		},
+		{
+			name:     "1 neighbour above",
+			input:    [][]int{
+				{1},
+				{0},
+			},
+			row:      1,
+			col:      0,
+			expected: 1,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			game := life.World{Grid: tc.input}
+
+			assert.Equal(t, tc.expected, gol.Neighbours(game, tc.row, tc.col))
+		})
+	}
+}
